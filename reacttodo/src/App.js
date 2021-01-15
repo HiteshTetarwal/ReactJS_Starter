ab47f4ce8bf8679c228f96644fbc36f57ed41628
HiteshTetarwal <imhitesh007@gmail.com>
2021-01-16 01:57:13 +0530

    frontend starter with CSS

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './LCO-Logo-White.png';
+import "./App.css";
 
 
-function App(){
-  return (
-    <div className="App">
-    <header className="App-header">
-    <img src={logo} className="App-logo" />
-    <p>Learn React with Hitesh Crash course</p>
-    </header>
-    </div>
+
+class App extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <img src={logo} width="100" height="100" className="logo"/>
+        <h1 className="app-title">React ToDo App</h1>
+        <div className="container">
+          Add an Item....
+          <br/>
+          <input type="text" className="input-text" placeholder="Write a ToDo"></input>
+          <button className="add-btn">Add Button</button>
+          <div className="list">
+            <ul>
+              <li>
+                <input type="checkbox" name="" id=""/>
+                Record youtube videos
+                <button className="btn">Delete</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
+  }
+
 }
 
-export default App;
+export default App
